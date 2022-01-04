81fa798928941aa8c7b301b4dad8ad1e87e532b3
Jezzzae <Jessicavences65@gmail.com>
2022-01-04 16:17:23 -0600

    genre model done

--- bookstoreapp/src/main/java/com/bookstoreapp/model/Book.java
+++ bookstoreapp/src/main/java/com/bookstoreapp/model/Book.java
@@ -1,62 +1,5 @@
 package com.bookstoreapp.model;
 
-<<<<<<< HEAD
-@Entity
-@Table(name = "books")
-public class Book {
-
-    @Id
-    @Column
-    private Long id;
-
-    @Column
-    private String bookTitle;
-
-    @Column
-    private int pageLength;
-
-    //do i include foreign keys ??
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBookTitle() {
-        return bookTitle;
-    }
-
-    public void setBookTitle(String bookTitle) {
-        this.bookTitle = bookTitle;
-    }
-
-    public int getPageLength() {
-        return pageLength;
-    }
-
-    public void setPageLength(int pageLength) {
-        this.pageLength = pageLength;
-    }
-
-    public Book(Long id, String bookTitle, int pageLength,){
-        this.id = id;
-        this.bookTitle = bookTitle;
-        this.pageLength = pageLength;
-    }
-    public Book(){
-        //default ctor
-    }
-
-    
-    //fav list
-    //genre
-    //publisher
-    //author
-
-
-=======
 
 import javax.persistence.*;
 import java.util.Set;
@@ -74,5 +17,4 @@ public class Book {
 
     @ManyToMany
     Set<Author> authors;
->>>>>>> a8ccdac4af21f7dc76a6212a9e6f92a1247ce8ec
 }
--- bookstoreapp/src/main/java/com/bookstoreapp/model/Genre.java
+++ bookstoreapp/src/main/java/com/bookstoreapp/model/Genre.java
@@ -1,4 +1,55 @@
 package com.bookstoreapp.model;
 
+@Enitiy
+@Table (name = "Genre")
 public class Genre {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private  String name;
+
+    @Column
+    private  String subGenre;
+
+    @ManyToMany
+    Set<Book>books;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubGenre() {
+        return subGenre;
+    }
+
+    public void setSubGenre(String subGenre) {
+        this.subGenre = subGenre;
+    }
+
+    public Genre (Long id, String name, String subGenre){
+        this.id = id;
+        this.name = name;
+        this.subGenre = subGenre;
+    }
+
+    public Genre(){
+        //default
+    }
+
 }
