d228260af6c4d0a990bda8d4aa4cb6da2b0baee5
Jezzzae <Jessicavences65@gmail.com>
2022-01-04 16:52:31 -0600

    added usermodel

--- bookstoreapp/src/main/java/com/bookstoreapp/model/User.java
+++ bookstoreapp/src/main/java/com/bookstoreapp/model/User.java
@@ -1,14 +1,51 @@
-//package com.bookstoreapp.model;
-//
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import org.hibernate.annotations.LazyCollection;
-//import org.hibernate.annotations.LazyCollectionOption;
-//
-//import javax.persistence.*;
-//import java.util.List;
-//
-//@Entity
-//@Table(name="users")
-//public class User {
-//
-//}
+package com.bookstoreapp.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @Column
+    private  Long userId;
+
+    @Column
+    private String userName;
+
+    @OneToOne
+    Set<List> lists;
+
+    public User(Long userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public User (){
+        //default
+
+
+    }
+
+}
