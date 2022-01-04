947d9f524f231570bfb8e20aa9926035acb4e016
Jezzzae <Jessicavences65@gmail.com>
2022-01-04 14:55:23 -0600

    constructors

--- bookstoreapp/src/main/java/com/bookstoreapp/model/Book.java
+++ bookstoreapp/src/main/java/com/bookstoreapp/model/Book.java
@@ -3,6 +3,18 @@ package com.bookstoreapp.model;
 @Entity
 @Table(name = "books")
 public class Book {
+
+    @Id
+    @Column
+    private Long id;
+
+    @Column
+    private String bookTitle;
+
+    @Column
+    private int pageLength;
+
+    //do i include foreign keys ??
     public Long getId() {
         return id;
     }
@@ -27,16 +39,14 @@ public class Book {
         this.pageLength = pageLength;
     }
 
-    @Id
-    @Column
-    private Long id;
+    public Book(Long id, String bookTitle, int pageLength,){
+        this.id = id;
+        this.bookTitle = bookTitle;
+        this.pageLength = pageLength;
+    }
+    public Book(){
+        //default ctor
+    }
 
-    @Column
-    private String bookTitle;
-
-    @Column
-    private int pageLength;
-
-    //do i include foreign keys ??
 
 }
