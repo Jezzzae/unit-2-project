e92572f34cbfdfea90f076064695f9406323ddf3
Jezzzae <Jessicavences65@gmail.com>
2022-01-04 14:21:37 -0600

    book variables
    q
    q
    
    Q
    Git
    git add .
    ls
    cd ..
    git commit -me

--- bookstoreapp/src/main/java/com/bookstoreapp/model/Book.java
+++ bookstoreapp/src/main/java/com/bookstoreapp/model/Book.java
@@ -1,4 +1,42 @@
 package com.bookstoreapp.model;
 
+@Entity
+@Table(name = "books")
 public class Book {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public int getPageLength() {
+        return pageLength;
+    }
+
+    public void setPageLength(int pageLength) {
+        this.pageLength = pageLength;
+    }
+
+    @Id
+    @Column
+    private Long id;
+
+    @Column
+    private String bookTitle;
+
+    @Column
+    private int pageLength;
+
+    //do i include foreign keys ??
+
 }
