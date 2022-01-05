a25234433b3f2173db434e18449a97c6aebc4ae9
Sheree <sheree.edmund@gmail.com>
2022-01-05 15:33:51 -0500

    pulling controller down

--- bookstoreapp/src/main/java/com/bookstoreapp/repository/BookRepository.java
+++ bookstoreapp/src/main/java/com/bookstoreapp/repository/BookRepository.java
@@ -4,7 +4,7 @@ import com.bookstoreapp.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-//testing fddfddff
+//testing 1234567
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByTitle(String title);
 }
