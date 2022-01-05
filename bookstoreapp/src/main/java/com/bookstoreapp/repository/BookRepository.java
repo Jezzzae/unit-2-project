ab3c3d3b44b1333d29fb6edc928e1d137f111142
Sheree <sheree.edmund@gmail.com>
2022-01-05 14:42:09 -0500

    BookRepository.Java

--- bookstoreapp/src/main/java/com/bookstoreapp/repository/BookRepository.java
+++ bookstoreapp/src/main/java/com/bookstoreapp/repository/BookRepository.java
@@ -4,7 +4,7 @@ import com.bookstoreapp.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-
+//testing
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByTitle(String title);
 }
